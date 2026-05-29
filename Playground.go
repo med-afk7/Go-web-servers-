ad6182ea31232746064444d5117e166f0c095944
ahmed arman <ahmedwaleedarman7@gmail.com>
2026-05-29 19:48:20 -0400

    intial

---
+++ Playground.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+
+	http.HandleFunc("/", helloHandleFunc)
+	http.ListenAndServe(":8080", nil)
+
+}
+
+func helloHandleFunc(w http.ResponseWriter, r *http.Request) {
+	a := 10
+	b := 20
+	fmt.Fprintf(w, "Hello , World! %v", a+b)
+}
